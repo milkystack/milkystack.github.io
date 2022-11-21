bd98c4c82abb068e54b66f66a18de7eefa3957ef
milkystack <milkystack@gmail.com>
2022-11-22 00:15:37 +0900

    2022/11/22 pink

--- script_music.js
+++ script_music.js
@@ -8,7 +8,7 @@ request.onload = function() {
 
     for (var index = 0; index < list.length; index++) {
         var murl = list[index].replace('music.apple.com', 'embed.music.apple.com');
-        var elem = '<iframe src="'+murl+'&amp;app=music&amp;itsct=music_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto" height="175px" frameborder="0" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" allow="autoplay *; encrypted-media *; clipboard-write" style="width: 100%; overflow: hidden; border-radius: 10px; background-color: transparent; --darkreader-inline-bgimage: initial; --darkreader-inline-bgcolor: transparent;" data-darkreader-inline-bgimage="" data-darkreader-inline-bgcolor=""></iframe>'
+        var elem = '<iframe src="'+murl+'&amp;app=music&amp;itsct=music_box_player&amp;itscg=30200&amp;ls=1&amp;theme=light" height="175px" frameborder="0" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" allow="autoplay *; encrypted-media *; clipboard-write" style="width: 100%; overflow: hidden; border-radius: 10px; background-color: transparent; --darkreader-inline-bgimage: initial; --darkreader-inline-bgcolor: transparent;" data-darkreader-inline-bgimage="" data-darkreader-inline-bgcolor=""></iframe>'
         playlist.insertAdjacentHTML('beforeend', elem); 
     }
 }
